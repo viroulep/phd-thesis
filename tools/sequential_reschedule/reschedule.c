0a363ca731307901a2999d0c6f96531a956264f4
Philippe Virouleau <philippe.virouleau@imag.fr>
2016-06-08 17:44:37 +0200

    First try for task ids.
    Need to fix call from fortran to C.

--- tools/sequential_reschedule/reschedule.c
+++ tools/sequential_reschedule/reschedule.c
@@ -6,10 +6,17 @@
 
 unsigned long long ntasks = 0;
 
+typedef struct task_id_t {
+    int t;
+    int x;
+    int y;
+    int z;
+} task_id_t;
+
 typedef struct task_t {
     void (*fn)(void *);
     void *args;
-    int priority;
+    task_id_t id;
 } task_t;
 
 task_t **tasks;
@@ -25,6 +32,17 @@ omp_set_priority_(int titi)
 {
 }
 
+task_id_t current_id;
+
+void
+omp_set_taskid(int t, int x, int y, int z)
+{
+    current_id.t = t;
+    current_id.x = x;
+    current_id.y = y;
+    current_id.z = z;
+}
+
 void
 GOMP_task (void (*fn) (void *), void *data, void (*cpyfn) (void *, void *),
 	   long arg_size, long arg_align, bool if_clause, unsigned flags,
@@ -38,19 +56,17 @@ GOMP_task (void (*fn) (void *), void *data, void (*cpyfn) (void *, void *),
         /*char buf[arg_size + arg_align - 1];*/
         char *arg = (char *) (((uintptr_t) dynargs + arg_align - 1)
                 & ~(uintptr_t) (arg_align - 1));
-        /*printf("caca, %lu\n", arg_size);*/
         cpyfn (arg, data);
         task->args = arg;
         task->fn = fn;
-        task->priority = priority;
+        task->id = current_id;
         /*fn(arg);*/
     }
     else {
-        /*printf("coucou, %lu\n", arg_size);*/
         memcpy(dynargs, data, arg_size);
         task->args = dynargs;
         task->fn = fn;
-        task->priority = priority;
+        task->id = current_id;
 		/*fn(data);*/
     }
     tasks[ntasks] = task;
@@ -66,7 +82,10 @@ GOMP_parallel (void (*fn) (void *), void *data, unsigned num_threads, unsigned i
     fn(data);
     printf("End of a region, %llu tasks\n", ntasks);
     printf("Rexecuting stuff:\n");
-    for (unsigned int i = 0; i < ntasks; i++)
+    for (unsigned int i = 0; i < ntasks; i++) {
+        task_id_t *id = &tasks[i]->id;
+        /*printf("Executing id (t,x,y,z) = (%i,%i,%i,%i)\n", id->t, id->x, id->y, id->z);*/
         tasks[i]->fn(tasks[i]->args);
+    }
 }
 
